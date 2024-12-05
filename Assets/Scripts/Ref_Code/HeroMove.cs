9125240fbbd77ecb0047e9e194653f74cd70c17e
Jack4me <just4go@gmail.com>
2024-12-05 16:36:29 +0100

    +3

--- Assets/Scripts/Ref_Code/HeroMove.cs
+++ Assets/Scripts/Ref_Code/HeroMove.cs
@@ -6,9 +6,12 @@ namespace Ref_Code
 {
     public class HeroMove : MonoBehaviour
     {
-        public float movementSpeed = 4.0f;
+        public float speed = 4.0f;
         private IInputService _inputService;
         private Camera _camera;
+        private Vector2 velocity;
+        [SerializeField] private float rotationSpeed = 20f;
+        [SerializeField] private float acceleration;
 
         public HeroMove Construct(){
             return this;
@@ -18,12 +21,50 @@ namespace Ref_Code
             _inputService = AllServices.Container.GetService<IInputService>();
         }
         private void Update(){
-            Vector3 movementVector = Vector3.zero;
             if (_inputService.Axis.sqrMagnitude > 0.01f){
                 Debug.Log("_inputService.Axis" + _inputService.Axis);
+                HandleMovement(_inputService.Axis);
+            }
+            else
+            {
+                ApplyVelocity();
             }
            
         }
+        public void HandleMovement(Vector2 _inputService)
+        {
+            Rotate(_inputService.x);
+            Accelerate(_inputService.y);
+            Move();
+        }
 
+        private void Rotate(float rotationInput)
+        {
+            float rotation = -rotationInput * rotationSpeed * Time.deltaTime;
+            transform.Rotate(0f, 0f, rotation);
+        }
+        public void ApplyVelocity()
+        {
+            transform.position += (Vector3)velocity * Time.deltaTime;
+        }
+        public void AddVelocity(Vector2 direction, float force)
+        {
+            velocity += direction * force;
+        }
+        private void Accelerate(float accelerationInput)
+        {
+            velocity += (Vector2)transform.up * accelerationInput * acceleration;
+
+            if (velocity.magnitude > speed)
+            {
+                velocity = velocity.normalized * speed;
+            }
+        }
+
+        private void Move()
+        {
+            transform.position += (Vector3)velocity * Time.deltaTime;
+        }
+       
     }
 }
