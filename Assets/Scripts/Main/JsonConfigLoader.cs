b3f70addbebebbd506f52caba046a506ce0bc786
Jack4me <just4go@gmail.com>
2025-01-29 12:37:01 +0100

    startAsmdf

--- Assets/Scripts/Main/JsonConfigLoader.cs
+++ Assets/Scripts/Main/JsonConfigLoader.cs
@@ -8,58 +8,51 @@ namespace Main
     {
         private HeroMoveConfig _configs;
 
-       
         public HeroMoveConfig LoadConfigsHero()
         {
-            //_staticData.LoadStaticData();
             TextAsset jsonFile = Resources.Load<TextAsset>("HeroMoveConfig");
             if (jsonFile != null)
             {
-             return  JsonUtility.FromJson<HeroMoveConfig>(jsonFile.text);
+                return JsonUtility.FromJson<HeroMoveConfig>(jsonFile.text);
             }
-            else
-            {
-                Debug.LogError("JSON файл не найден в папке Resources");
-            }
+
+            Debug.LogError("JSON файл не найден в папке Resources");
             return null;
         }
+
         public UFOConfig LoadConfigsEnemy()
         {
             TextAsset jsonFile = Resources.Load<TextAsset>("UFOConfig");
             if (jsonFile != null)
             {
-                return  JsonUtility.FromJson<UFOConfig>(jsonFile.text);
+                return JsonUtility.FromJson<UFOConfig>(jsonFile.text);
             }
-            else
-            {
-                Debug.LogError("JSON файл не найден в папке Resources");
-            }
+
+            Debug.LogError("JSON файл не найден в папке Resources");
             return null;
         }
+
         public LaserControllerConfig LoadConfigLaser()
         {
             TextAsset jsonFile = Resources.Load<TextAsset>("LaserControllerConfig");
             if (jsonFile != null)
             {
-                return  JsonUtility.FromJson<LaserControllerConfig>(jsonFile.text);
+                return JsonUtility.FromJson<LaserControllerConfig>(jsonFile.text);
             }
-            else
-            {
-                Debug.LogError("JSON файл не найден в папке Resources");
-            }
+
+            Debug.LogError("JSON файл не найден в папке Resources");
             return null;
         }
+
         public LaserStatsConfig LoadStatsConfigLaser()
         {
             TextAsset jsonFile = Resources.Load<TextAsset>("LaserStatsConfig");
             if (jsonFile != null)
             {
-                return  JsonUtility.FromJson<LaserStatsConfig>(jsonFile.text);
+                return JsonUtility.FromJson<LaserStatsConfig>(jsonFile.text);
             }
-            else
-            {
-                Debug.LogError("JSON файл не найден в папке Resources");
-            }
+
+            Debug.LogError("JSON файл не найден в папке Resources");
             return null;
         }
     }
